743463209802233272e8b21c74d43eb1f409eb25
jimroxodezi <jimroxodezi@gmail.com>
2022-11-15 15:15:05 +0100

    updated

--- printmultvec.cc
+++ printmultvec.cc
@@ -3,21 +3,23 @@
 #include <algorithm>
 
  void print_vector(const std::vector<int> vec) {
-    for (std::vector<int>::const_iterator cit = vec.begin(); cit != vec.end(); ++cit)
+    for (std::vector<int>::const_iterator cit = vec.cbegin(); cit != vec.cend(); ++cit)
         std::cout << *cit << " ";
     std::cout << "\n";
  }
 
  int main(){
-    std::vector<int> v1(7,0);
-    std::vector<int> v2 = {1,2,3,4,5,6,7};
-    std::vector<int> v3 = {1,2,3,4,5,6,7};
-    std::vector<int> v4 = {1,2,3,4,5,6,7};
-    std::vector<int> v5 = {1,2,3,4,5,6,7};
-    std::vector<int> v6 = {1,2,3,4,5,6,7};
-    std::vector<int> v7 = {1,2,3,4,5,6,7};
+    auto v1 = std::vector(9,0);
+    auto v2 = std::vector{1,2,3,4,5,6,7,8,9};
+    auto v3 = std::vector{1,2,3,4,5,6,7,8,9};
+    auto v4 = std::vector{1,2,3,4,5,6,7,3,2};
+    auto v5 = std::vector{1,2,3,4,5,6,7,1,3};
+    auto v6 = std::vector{1,2,3,4,5,6,7,4,6};
+    auto v7 = std::vector{1,2,3,4,5,6,7,5,6};
+    auto v8 = std::vector{1,2,3,4,5,6,7,9,4};
+    auto v9 = std::vector(9,0);
 
-    std::vector<std::vector<int>> vec2d {v1,v2,v3,v4,v5,v6,v7};
+    std::vector<std::vector<int>> vec2d {v1,v2,v3,v4,v5,v6,v7,v8,v9};
 
     std::for_each(vec2d.cbegin(), vec2d.cend(), print_vector);
  }
