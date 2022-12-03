724477defbe50e30e30830b47c8001e4f617f9d8
jimroxodezi <jimroxodezi@gmail.com>
2022-12-03 08:38:47 +0100

    c++20

---
+++ c++20/concepts/sum.cc
@@ -0,0 +1,21 @@
+#include <concepts>
+#include <iostream>
+
+
+// concepts evaluates to bool?
+template <typename T>
+concept Number = std::is_same_v<T, int> || std::is_floating_point_v<T>;
+
+// template <Number Num>
+// Num sum(Num a, Num b) {
+//     return a + b;
+// }
+
+// same as above
+auto sum(Number auto a, Number auto b) {
+    return a + b;
+}
+
+int main(void) {
+    std::cout << sum(12,42) << std::endl;
+}
